3eb7ed2742508538f0f126970787acdba0b3b37d
Raul1978x <108808330+Raul1978x@users.noreply.github.com>
2023-09-24 18:05:03 -0300

    Create components: navbar, index, footer, login, register

--- Angular/BillMyCodeFrontendAngular/src/app/app.module.ts
+++ Angular/BillMyCodeFrontendAngular/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './Component/index/index.component';
+import { NavbarComponent } from './Component/navbar/navbar.component';
+import { FooterComponent } from './Component/footer/footer.component';
+import { LoginComponent } from './Component/login/login.component';
+import { RegisterComponent } from './Component/register/register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, IndexComponent, NavbarComponent, FooterComponent, LoginComponent, RegisterComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
